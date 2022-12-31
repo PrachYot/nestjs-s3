26b022ac044063befef982d7b7d4cbe8d0899fa4
Prach Yothaprasert <prach@oozou.com>
2022-12-31 23:10:42 +0700

    feat: create dynamic module

--- src/s3/s3.module.ts
+++ src/s3/s3.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { S3Service } from './s3.service';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-    }),
-  ],
-  providers: [S3Service],
-  exports: [S3Service],
-})
-export class S3Module {}
+@Module({})
+export class S3Module {
+  static register(options: { envFilePath: string }): DynamicModule {
+    const envPath = options.envFilePath || '.env';
+
+    return {
+      module: S3Module,
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: [envPath],
+        }),
+      ],
+      providers: [S3Service],
+      exports: [S3Service],
+    };
+  }
+}
